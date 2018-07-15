4f097b2fb8911a4a822459ba7c4f2a1f0ea1665c
Megapunkdev <32435920+Megapunkdev@users.noreply.github.com>
2018-07-15 14:59:39 +0200

    little modifications to styling

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflowY: 'scroll', border: '1px solid black', height: '1000px' }}>
+        <div style={{ overflowY: 'scroll', borderTop: '1px solid black', borderBottom: '1px solid black', height: '70vh', backgroundColor: 'rgba(255,255,255,.5)' }}>
             {props.children}
         </div>
     );
