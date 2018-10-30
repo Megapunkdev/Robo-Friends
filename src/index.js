c50fb14e5ffe135feca61d9ebb05f01a3274a591
Megapunkdev <32435920+Megapunkdev@users.noreply.github.com>
2018-10-30 08:42:27 +0100

    fixed things

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
